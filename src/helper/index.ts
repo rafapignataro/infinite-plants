1c1cd5f0652f6551197045e26535f1b42a87a21f
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-10-15 01:40:53 -0300

    Adding first steps of l-system

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,113 +1,128 @@
 import * as THREE from 'three';
 
-interface Plant {
-  axiom: string;
-  rules: any;
-  angle: number;
-}
+const generateSentence = (axiom: string, generations: number, rules: any) => {
+  let sentence = axiom;
 
-// F -> Draw line (line, translate)
-// G -> Move foward (translate)
-// + -> Turn right (rotate)
-// - -> Turn left (rotate)
-// [ -> Save state (push matrix)
-// ] -> End state (pop matrix)
+  for (let generation = 1; generation < generations; generation++) {
+    let nextSentence = '';
 
-const plant: Plant = {
-  axiom: 'F',
-  angle: Math.PI / 4,
-  rules: {
-    'F': 'FF-[F+F+F]+[+F-F-F]',
+    for (let index = 0; index < sentence.length; index++) {
+      const letter = sentence[index];
+      nextSentence += rules[letter] || letter;
+    }    
+
+    sentence = nextSentence;
   }
+
+  return sentence;
 }
 
+const getRandomColor = () =>{
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 export const setupGame = () => {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, 1280/720, 0.1, 1000);
-  
+
   const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(854, 480);
+  renderer.setSize(1200, 720);
 
-  // 'A'
-  let result = plant.axiom;
+  const AXIOM = 'F';
+  const BASE_ANGLE = Math.PI / 16;
+  const RULES: any = { 'F' : 'FF-[-F+F+F]+[+F-F-F]' };
+  const LENGTH = 3;
 
-  // Generations loop
-  for (let generation = 1; generation <= 3; generation++) {
-    let newString = '';
-    for (let index = 0; index < result.length; index++) {
-      const letter = result[index];
-
-      newString += plant.rules[letter] || letter;
-    }    
-
-    result = newString;
-    console.log('Generation: ', generation, 'String : ', result);
-  }
-
-  const drawList = [];
-
-  // Draw loop
-  for (let letter = 1; letter <= result.length; letter++) {
-    switch(result[letter]) {
-      case 'F': drawList.push('line');
-      case '+': drawList.push('right');
-      case '-': drawList.push('left');
-    }
-  }
-
-
-  let ANGLE = Math.PI * 0;
-  let SIZE = 3;
-
-  let generation = 0;
+  const SENTENCE = generateSentence(AXIOM, 2, RULES);
+  // Criar um array do helper position e helper angle
   
-  const Branch = ({ x, y, z }: any, size: number, angle: number) => {
-    const points = [];
-    generation++;
+  const Branch = (origin: { x: number, y: number, z: number }, length: number, angle: number) => {
+    let BRANCHING = false;
+    let ACTUAL_ANGLE = 0;
+    let HELPER_ANGLE = 0;
+    let ACTUAL_POSITION = origin;
+    let HELPER_POSITION = ACTUAL_POSITION;
+    let ANGLE_STACK = [];
+    let POSITION_STACK = [];
 
-    const originPoint = new THREE.Vector3(x, y, z);
+    for (let index = 0; index < SENTENCE.length; index++) {
+      const letter = SENTENCE[index];
+  
+      switch(letter) {
+        case 'F': {
+          const branchPoints = [];
 
-    const y1 = Math.cos(angle) * size;
-    const x1 = Math.sin(angle) * size;
-
-    const finalPoint = new THREE.Vector3(x1 + x, y1 + y, z);
-
-    points.push(originPoint);
-    points.push(finalPoint);
-
-    const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const material = new THREE.LineBasicMaterial({ 
-      color: '#e17055',
-    });
-    const branch = new THREE.Line(geometry, material);
-    
-    scene.add(branch);
-
-    const random1 = Math.PI / (Math.floor(Math.random() * 10)  + 4)
-    const random2 = Math.PI / (Math.floor(Math.random() * 10)  + 4)
-
-    if(size > 0.1) {
-      Branch({
-        x: x1 + x,
-        y: y1 + y,
-        z: 0
-      }, size / 2, (angle + random1))
-      Branch({
-        x: x1 + x,
-        y: y1 + y,
-        z: 0
-      }, size / 2, (angle - random2))
+          const initialPosition = BRANCHING ? HELPER_POSITION : ACTUAL_POSITION;
+          const finalPosition = { 
+            x: Math.sin(BRANCHING ? HELPER_ANGLE : ACTUAL_ANGLE) * length + initialPosition.x, 
+            y: Math.cos(BRANCHING ? HELPER_ANGLE : ACTUAL_ANGLE) * length + initialPosition.y, 
+            z: 0 
+          };
+          
+          const originPoint = new THREE.Vector3(initialPosition.x, initialPosition.y, initialPosition.z);
+          const finalPoint = new THREE.Vector3(finalPosition.x, finalPosition.y, finalPosition.z);
+          branchPoints.push(originPoint);
+          branchPoints.push(finalPoint);
+          const geometry = new THREE.BufferGeometry().setFromPoints(branchPoints);
+          const material = new THREE.LineBasicMaterial({ color: getRandomColor() });
+          const branch = new THREE.Line(geometry, material);
+          
+          scene.add(branch);
+          
+          if(BRANCHING) {
+            HELPER_POSITION = finalPosition;
+          } else {
+            ACTUAL_POSITION = finalPosition;
+          }
+          break;
+        }
+        case '+': {
+          if(BRANCHING) {
+            HELPER_ANGLE += angle;
+            ANGLE_STACK.push(HELPER_ANGLE);
+          } else {
+            ACTUAL_ANGLE += angle;
+          }
+          break;
+        }
+        case '-': {
+          if(BRANCHING) {
+            HELPER_ANGLE -= angle;
+            ANGLE_STACK.push(HELPER_ANGLE);
+          } else {
+            ACTUAL_ANGLE -= angle;
+          }
+          break;
+        }
+        case '[': {
+          BRANCHING = true;
+          HELPER_POSITION = ACTUAL_POSITION;
+          break;
+        }
+        case ']': {
+          BRANCHING = false;
+          HELPER_POSITION = ACTUAL_POSITION;
+          HELPER_ANGLE = 0;
+          break;
+        }
+      }
     }
   }
-
-  Branch({ x: 0, y: -3.5, z: 0 }, SIZE, ANGLE)
-
-  camera.position.z = 5;
-
+    
+  Branch({ x: 0, y: -15, z: 0 }, LENGTH, BASE_ANGLE);
+  
+  camera.position.z = 20;
   const gameContainer = document.querySelector('.game-container');
 
-  gameContainer?.appendChild(renderer.domElement);
+  if(gameContainer) {
+    gameContainer.innerHTML = '';
+    gameContainer.appendChild(renderer.domElement);
+  }
 
   const animate = () => {
     requestAnimationFrame(animate);
