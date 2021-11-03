39b61c8063da6512b1b4fa1a34959ba45b569201
Rafael Pignataro <rafael.pignataro@singu.com.br>
2021-11-03 03:06:08 -0300

    Refactored some code to fit react archicture

--- src/helper/infinitePlant.ts
+++ src/helper/infinitePlant.ts
@@ -22,45 +22,42 @@ type Rule = {
   sentence: string;
 }
 
+type Plant = {
+  axiom: string,
+  rules: Rule[],
+  angle: number,
+  branchSize: number,
+  generation: number,
+  sentences: { sentence: string, current: number }[]
+}
+
 class InfinitTree {
+  private plant: Plant;
+  // INTERNAL PLANT
   private initialPosition: Position;
   private baseBranch: Branch;
   private branches: Branch[] = [];
-  private axiom: string;
-  private angle: number;
-  private rules: Rule[];
-  private branchSize: number;
-  private generations: number;
   private scene: THREE.Scene;
-  private sentences: { sentence: string, current: number }[] = [];
-  private variables: any;
+  private options: any;
 
   constructor(
+    plant: Plant,
     initialPosition: Position, 
-    axiom: string, 
-    rules: any,
-    angle: number, 
-    branchSize: number, 
-    generations: number, 
     scene: THREE.Scene,
-    variables: any
+    options: any
   ) {
+    this.plant = plant;
+    // INTERNAL PLANT
     this.initialPosition = initialPosition;
-    this.axiom = axiom;
-    this.rules = rules;
-    this.angle = angle;
-    this.initialPosition = initialPosition;
-    this.branchSize = branchSize;
-    this.generations = generations;
     this.scene = scene;
-    this.variables = variables;
+    this.options = options;
 
     this.baseBranch = {
       id: 0,
-      angle: this.angle,
+      angle: plant.angle,
       rotationAngle: 0,
       points: [new THREE.Vector3(this.initialPosition.x, initialPosition.y, initialPosition.z)],
-      width: (this.generations / 4) - (0.1 * this.branches.length),
+      width: (plant.generation / 4) - (0.1 * this.branches.length),
       color: this.getBranchColor(this.branches.length)
     }
   }
@@ -75,85 +72,19 @@ class InfinitTree {
       ['#53350A', '#5c3a0a', '#4c6a2f', '#618A3C', '#618A3B', '#9b59b6'],
     ];
     
-    if(this.generations === 0) return colorsMatrix[0][number]
+    if(this.plant.generation === 0) return colorsMatrix[0][number]
 
-    return colorsMatrix[this.generations - 1][number];
+    return colorsMatrix[this.plant.generation - 1][number];
   }
 
   private drawBranch = (branch: Branch) => {
     const curve = new THREE.CatmullRomCurve3(branch.points);
     const branchGeometry = new THREE.TubeGeometry(curve, branch.points.length * 3, branch.width, 8, false);
-    const branchMaterial = new THREE.MeshPhysicalMaterial({ color: branch.color, side: THREE.DoubleSide, wireframe: this.variables.wireframe  });
+    const branchMaterial = new THREE.MeshPhysicalMaterial({ color: branch.color, side: THREE.DoubleSide, wireframe: this.options.wireframe  });
     const branchMesh = new THREE.Mesh(branchGeometry, branchMaterial);
     branchMesh.castShadow = true
     this.scene.add(branchMesh);
   }
-
-  private generateSentence = (pastSentence: { sentence: string, current: number } | null) => {
-    let nextSentence = '';
-
-    if(!pastSentence) return {
-      sentence: this.axiom,
-      current: 0
-    }
-
-    for (let letterIndex = 0; letterIndex < pastSentence.sentence.length; letterIndex++) {
-      const letter = pastSentence.sentence[letterIndex];
-
-      const rules = this.rules.filter(rule => rule.letter === letter);
-
-      switch(rules.length) {
-        case 0:
-          nextSentence += letter;
-          break;
-        case 1:
-          nextSentence += rules[0].sentence;
-          break;
-        default: 
-          // Multiple rules, random sentence
-          const random = Number(Math.random().toFixed(2));
-
-          let testText = '';
-          let initialRange = 0;
-          let selectedRule = null;
-
-          if(random === 1) {
-            selectedRule = rules[rules.length - 1];
-            break;
-          }
-
-          for (let ruleIndex = 0; ruleIndex < rules.length; ruleIndex++) {
-            const rule = rules[ruleIndex];
-
-            const finalRange = initialRange + rule.odds;
-
-            testText += `${initialRange}-${finalRange} `;
-
-            if(random >= initialRange && random < finalRange) {
-              selectedRule = rule;
-              break;
-            }
-            
-            initialRange = finalRange;
-          }
-
-          if(!selectedRule) {
-            console.log(testText)
-            console.log(initialRange, random)
-            console.error('Rule does not exists')
-          }
-
-          nextSentence += selectedRule?.sentence;
-
-          break;
-      }
-    }    
-    
-    return {
-      sentence: nextSentence,
-      current: 0
-    }
-  };
 
   public growStep = (letter: string) => {
     const stack_branch = this.branches[this.branches.length - 1];
@@ -180,11 +111,11 @@ class InfinitTree {
       if(rotationAngle >= 270 && rotationAngle < 360) return { z: z * -1, x }
 
       return { z: z * -1, x }
-    })(branch.rotationAngle, this.branchSize);
+    })(branch.rotationAngle, this.plant.branchSize);
 
     let finalPosition = {
       x: 0,
-      y: Math.cos(branch.angle) * this.branchSize + initialPosition.y,
+      y: Math.cos(branch.angle) * this.plant.branchSize + initialPosition.y,
       z: 0
     }
 
@@ -208,13 +139,13 @@ class InfinitTree {
         break;
       }
       case '+': {
-        branch.angle += this.angle;
-        branch.rotationAngle += this.angle;
+        branch.angle += this.plant.angle;
+        branch.rotationAngle += this.plant.angle;
         break;
       }
       case '-': {
-        branch.angle -= this.angle;
-        branch.rotationAngle += this.angle;
+        branch.angle -= this.plant.angle;
+        branch.rotationAngle += this.plant.angle;
         break;
       }
       case '[': {
@@ -230,7 +161,7 @@ class InfinitTree {
           id: this.branches.length + 1,
           points: [branchLastPoint],
           rotationAngle,
-          width: (this.generations / 10) - (0.1 * this.branches.length),
+          width: (this.plant.generation / 10) - (0.1 * this.branches.length),
           color: this.getBranchColor(this.branches.length)
         }
 
@@ -250,33 +181,86 @@ class InfinitTree {
     }
   }
 
-  public grow = (generation = 0) => {
-    for (let generationIndex = 0; generationIndex <= generation; generationIndex++) {
-      const pastSentence = this.sentences[generationIndex - 1];
-      const sentence = this.generateSentence(pastSentence);
-
-      console.log('----------')
-      // while(sentence.current < sentence.sentence.length) {
-      //   const letter = sentence.sentence[sentence.current];
-      
-      //   sentence.current += 1;
-      // }
-
-      this.sentences.push(sentence);
-    }
-
-    const { sentence } = this.sentences[this.sentences.length - 1];
+  public grow = () => {
+    const { sentence } = this.plant.sentences[this.plant.generation - 1] || { sentence: 'F', current: 0 };
 
     for(let letterIndex = 0; letterIndex < sentence.length; letterIndex++) {
       this.growStep(sentence[letterIndex]) 
     }
 
     this.drawBranch(this.baseBranch);
-    console.log(this.sentences);
   }
 }
 
-export const setupGame = (variables: any) => {
+export const generateSentence = (plant: Plant) => {
+  const pastSentence = plant.sentences[plant.sentences.length - 1];
+  let nextSentence = '';
+
+  if(!pastSentence) return {
+    sentence: plant.axiom,
+    current: 0
+  }
+
+  for (let letterIndex = 0; letterIndex < pastSentence.sentence.length; letterIndex++) {
+    const letter = pastSentence.sentence[letterIndex];
+
+    const rules = plant.rules.filter(rule => rule.letter === letter);
+
+    switch(rules.length) {
+      case 0:
+        nextSentence += letter;
+        break;
+      case 1:
+        nextSentence += rules[0].sentence;
+        break;
+      default: 
+        // Multiple rules, random sentence
+        const random = Number(Math.random().toFixed(2));
+
+        let testText = '';
+        let initialRange = 0;
+        let selectedRule = null;
+
+        if(random === 1) {
+          selectedRule = rules[rules.length - 1];
+          break;
+        }
+
+        for (let ruleIndex = 0; ruleIndex < rules.length; ruleIndex++) {
+          const rule = rules[ruleIndex];
+
+          const finalRange = initialRange + rule.odds;
+
+          testText += `${initialRange}-${finalRange} `;
+
+          if(random >= initialRange && random < finalRange) {
+            selectedRule = rule;
+            break;
+          }
+          
+          initialRange = finalRange;
+        }
+
+        if(!selectedRule) {
+          console.info(testText)
+          console.info(initialRange, random)
+          console.error('Rule does not exists')
+        }
+
+        nextSentence += selectedRule?.sentence;
+
+        break;
+    }
+  }    
+  
+  return {
+    sentence: nextSentence,
+    current: 0
+  }
+};
+
+
+export const createPlantScene = (plant: Plant, options: any) => {
   const gameContainer = document.querySelector('.game-container');
 
   if(!gameContainer) return;
@@ -285,46 +269,40 @@ export const setupGame = (variables: any) => {
 
   const scene = new THREE.Scene();
   scene.background = new THREE.Color('#74b9ff');
-  // scene.fog = new THREE.Fog( 0xa0a0a0, 30, 50 );
+
+  const GENERATION = plant.generation;
+
+  // GROUND
+  (() => {
+    const floorMesh = new THREE.Mesh(
+      new THREE.CircleGeometry(2000, 10),
+      new THREE.MeshPhongMaterial({ color: '#6ab04c', side: THREE.DoubleSide, depthWrite: false })
+    );
+    floorMesh.position.setY(GENERATION !== 0 ? GENERATION * -GENERATION : 0);
+    floorMesh.rotation.x = - Math.PI / 2;
+    floorMesh.receiveShadow = true;
+    scene.add(floorMesh);
+  })();
+
+  const INITIAL_POSITION: Position = { x: 0, y: (GENERATION * -GENERATION), z: 0 };
+
+  const tree = new InfinitTree(plant,INITIAL_POSITION, scene, options);
+
+  tree.grow();
+
+  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+  hemiLight.position.set( 0, 15, 0 );
+  scene.add(hemiLight);
   
+  camera.position.z = GENERATION !== 0 ? 15 * (GENERATION * 1) : 15;
+  camera.position.y = 0;
+
   const renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize(gameContainer.clientWidth, gameContainer?.clientHeight);
   renderer.shadowMap.enabled = true;
 
   const controls = new OrbitControls(camera, renderer.domElement);
-
-  const GENERATIONS = variables.generation;
-  const AXIOM = 'F';
-  const BASE_ANGLE = Math.PI / 10;
-  const BRANCH_SIZE = 1;
-  const INITIAL_POSITION: Position = { x: 0, y: (GENERATIONS * -GENERATIONS), z: 0 };
-  const RULES: Rule[] = [];
-
-  RULES.push({ odds: 0.4, letter: 'F', sentence: 'F[+FF]F' });
-  RULES.push({ odds: 0.3, letter: 'F', sentence: 'F[−FF]F' });
-  RULES.push({ odds: 0.3, letter: 'F', sentence: 'F[+FF+FF]+[−FF-FF]FF-' });
-
-  const floorMesh = new THREE.Mesh(
-    new THREE.CircleGeometry(2000, 10),
-    new THREE.MeshPhongMaterial({ color: '#6ab04c', side: THREE.DoubleSide, depthWrite: false })
-  );
-  
-  floorMesh.position.setY(GENERATIONS * -GENERATIONS);
-  floorMesh.rotation.x = - Math.PI / 2;
-  floorMesh.receiveShadow = true;
-  scene.add(floorMesh);
-
-  const tree = new InfinitTree(INITIAL_POSITION, AXIOM, RULES, BASE_ANGLE, BRANCH_SIZE, GENERATIONS, scene, variables);
-
-  tree.grow(GENERATIONS);
-
-  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-  hemiLight.position.set( 0, 15, 0 );
-  scene.add(hemiLight);
-  
-  camera.position.z = 15 * (GENERATIONS * 1);
-  camera.position.y = 0;
 
   if(gameContainer) {
     gameContainer.innerHTML = '';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,80 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect, useState } from 'react';
 
-import { setupGame } from '../helper';
+import { createPlantScene, generateSentence } from '../helper/infinitePlant';
+
+type Rule = {
+  odds: number;
+  letter: string;
+  sentence: string;
+}
+
+type Plant = {
+  axiom: string,
+  rules: Rule[],
+  angle: number,
+  branchSize: number,
+  generation: number,
+  sentences: { sentence: string, current: number }[]
+}
+
+type Branch = {
+  id: number;
+  angle: number;
+  rotationAngle: number;
+  points: THREE.Vector3[];
+  width: number;
+  color: string;
+}
+
+type Position = {
+  x: number
+  y: number;
+  z: number;
+}
+
+
+const RULES: Rule[] = [];
+RULES.push({ odds: 0.4, letter: 'F', sentence: 'F[+FF]F' });
+RULES.push({ odds: 0.3, letter: 'F', sentence: 'F[−FF]F' });
+RULES.push({ odds: 0.3, letter: 'F', sentence: 'F[+FF+FF]+[−FF-FF]FF-' });
+
+const Plant1: Plant = {
+  axiom: 'F',
+  rules: RULES,
+  angle: Math.PI / 10,
+  branchSize: 1,
+  generation: 0,
+  sentences: []
+}
 
 const Home: NextPage = () => {
-  const [variables, setVariables] = useState({
-    generation: 1,
+  const [options, setOptions] = useState({
     wireframe: false
   });
+  const [plant, setPlant] = useState<Plant>(Plant1);
 
   useEffect(() => {
-    setupGame(variables);
-  }, [variables]);
+    console.log("SENTENCES", plant.sentences)
+    createPlantScene(plant, options);
+  }, [plant, options]);
+
+  const handleChangeGeneration = (generation: number) => {
+    let sentences = plant.sentences;
+
+    if(generation > sentences.length) {
+
+      const newSentence = generateSentence(plant);
+
+      sentences.push(newSentence);
+    }
+
+    setPlant({
+      ...plant,
+      sentences,
+      generation,
+    })
+  }
 
   return (
     <>
@@ -24,24 +87,21 @@ const Home: NextPage = () => {
           <h1>Infinite Plants</h1>
           <h2>Variables</h2>
           <hr />
-          <p>Generation: {variables.generation}</p>
+          <p>Generation: {plant.generation}</p>
           <input 
             type="range" 
-            min="1" 
+            min="0" 
             max="5" 
             step="1"
-            value={variables.generation} 
-            onChange={(event) => setVariables({
-              ...variables,
-              generation: Number(event.target.value)
-            })}/>
+            value={plant.generation} 
+            onChange={(event) => handleChangeGeneration(Number(event.target.value))}/>
           <hr />
-          <p>Wireframe: {variables.generation}</p>
+          <p>Wireframe: {plant.generation}</p>
           <input 
             type="checkbox" 
-            checked={variables.wireframe} 
-            onChange={(event) => setVariables({
-              ...variables,
+            checked={options.wireframe} 
+            onChange={(event) => setOptions({
+              ...options,
               wireframe: event.target.checked
             })}/>
         </div>
